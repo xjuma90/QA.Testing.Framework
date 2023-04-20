12f331f68fad0f22c5ef8939ccbda07d3f735110
Julio Marin <julio.marin@arrivia.com>
2023-04-20 11:11:18 -0600

    adding waits

--- QA.Testing.Framework/PageObjects/SearchPage.cs
+++ QA.Testing.Framework/PageObjects/SearchPage.cs
@@ -1,6 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using QA.Testing.Framework.Hooks;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -23,11 +25,15 @@ namespace QA.Testing.Framework.PageObjects
 
         public void ClickSignupButton()
         {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(signUpBtn));
             driver.FindElement(signUpBtn).Click();
         }
 
         public void ValidateSignUpPopUpIsDisplayed()
         {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+            wait.Until(ExpectedConditions.VisibilityOfAllElementsLocatedBy(signUpTitle));
             Assert.IsTrue(driver.FindElement(signUpTitle).Displayed, "Sign up pop up is not displayed ");
         }
 
